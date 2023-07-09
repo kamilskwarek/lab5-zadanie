47b2fb180e8859cf8d655dd0c7b334ccf0eaf86e
kamilskwarek <kamilskwarek@interia.pl>
2023-07-09 18:15:39 +0200

    init

---
+++ Lab 5/TaskController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using static Lab_5.TaskMenager;
+
+namespace Lab_5.Controllers
+{
+    [ApiController]
+    [Route("api/tasks")]
+    public class TaskController : ControllerBase
+    {
+        private readonly TaskManager _taskManager;
+
+        public TaskController(TaskManager taskManager)
+        {
+            _taskManager = taskManager;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Task>> GetAllTasks()
+        {
+            List<Task> tasks = _taskManager.GetTasks();
+            return Ok(tasks);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Task> GetTaskById(int id)
+        {
+            Task task = _taskManager.GetTaskById(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public ActionResult<Task> CreateTask(Task task)
+        {
+            _taskManager.AddTask(task);
+            return CreatedAtAction(nameof(GetTaskById), new { id = task.Id }, task);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateTask(int id, Task updatedTask)
+        {
+            bool success = _taskManager.UpdateTask(id, updatedTask);
+            if (success)
+            {
+                return NoContent();
+            }
+            return NotFound();
+        }
+
+        [HttpPatch("{id}/complete")]
+        public ActionResult MarkTaskAsCompleted(int id)
+        {
+            bool success = _taskManager.MarkTaskAsCompleted(id);
+            if (success)
+            {
+                return NoContent();
+            }
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteTask(int id)
+        {
+            bool success = _taskManager.DeleteTask(id);
+            if (success)
+            {
+                return NoContent();
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ TestLab_5/TaskMenagerTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab_5; // Dodane
+using static Lab_5.TaskMenager;
+
+namespace TestLab_5
+{
+    public class TaskMenagerTest
+    {
+        private TaskManager _taskManager;
+
+        [SetUp]
+        public void Setup()
+        {
+            _taskManager = new TaskManager();
+        }
+
+        [Test]
+        public void AddTask_ShouldIncreaseTaskCount()
+        {
+            // Arrange
+            var task = new Lab_5.Task("Test task");
+            // Act
+            _taskManager.AddTask(task);
+            // Assert
+            Assert.AreEqual(1, _taskManager.GetTasks().Count);
+        }
+
+        [Test]
+        public void GetTasks_ShouldReturnAllAddedTasks()
+        {
+            // Arrange
+            var task1 = new Lab_5.Task("Task 1");
+            var task2 = new Lab_5.Task("Task 2");
+
+            _taskManager.AddTask(task1);
+            _taskManager.AddTask(task2);
+
+            // Act
+            var tasks = _taskManager.GetTasks();
+
+            // Assert
+            Assert.Contains(task1, tasks);
+            Assert.Contains(task2, tasks);
+            Assert.AreEqual(2, tasks.Count);
+        }
+
+        [Test]
+        public void RemoveTask_ShouldDecreaseTaskCountForExistingTask()
+        {
+            // Arrange
+            var task = new Lab_5.Task("Task");
+            _taskManager.AddTask(task);
+
+            // Act
+            bool result = _taskManager.RemoveTask(task.Id);
+
+            // Assert
+            Assert.IsTrue(result);
+            Assert.AreEqual(0, _taskManager.GetTasks().Count);
+        }
+    }
+}
